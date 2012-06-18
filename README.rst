045abf8167d6e36c668f946569b353d8b5697762
giginet <giginet.net@gmail.com>
2012-06-19 06:53:30 +0900

    Add documantation how to install for OS X.

--- README.rst
+++ README.rst
@@ -6,9 +6,14 @@ Instlation for Mac OS X
 
 1. Install Haskell Platform for Mac
 ------------------------------------
+http://hackage.haskell.org/platform/mac.html
 
 2. Run setup_darwin.sh
 -----------------------
+cd /path/to/PaperPuppet/
+sh util/setup_darwine.sh
 
 3. Set SDL_VIDEODRIVER as environment variable.
 -----------------------------------------------
+
+export SDL_VIDEODRIVER=X11
--- util/setup_darwin.sh
+++ util/setup_darwin.sh
@@ -3,17 +3,18 @@ if [[ "$unamestr" == 'Darwin' ]]; then
   ROOT=$(cd $(dirname $0);pwd) 
   /usr/bin/ruby -e "$(/usr/bin/curl -fsSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)"
   brew install sdl, sdl_image, sdl_mixer, sdl_ttf
-  git clone git@github.com:c255/PaperPuppet.git $ROOT
-  cd $ROOT/PaperPuppet
   cabal=`which cabal`
   if [[ "$cabal" == 'cabal not found' ]]; then
-    cabal install sdl, sdl-image, sdl-mixer, sdl-ttf
+    echo 'You must install Haskell Platform for Mac.'
+  else
+    cabal install sdl
+    cabal install sdl-image
+    cabal install sdl-mixer
+    cabal install sdl-ttf
     cabal configure
     cabal build
     echo 'Set SDL_VIDEODRIVER to X11 as environment variable.'
-    echo 'extern SDL_VIDEODRIVER=X11'
-  else
-    echo 'You must install Haskell Platform for Mac.'
+    echo 'export SDL_VIDEODRIVER=X11'
   fi
 else
   echo 'This script must be executed on Mac OS X.'
